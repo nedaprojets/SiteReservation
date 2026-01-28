3b9c1fe3d2f9cfbef8fa27a011b22ff60b791210
nedaprojets <nedagithubprojets@gmail.com>
2026-01-28 10:38:15 +0100

    initialization

---
+++ connexion/config.php
@@ -0,0 +1,9 @@
+<?php
+
+
+
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'coworker');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_CHARSET', 'utf8mb4');
---
+++ pages/salle.php
@@ -0,0 +1,51 @@
+<?php
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
+
+
+<div class="container">
+
+    <form action="">
+
+
+        <div class="mb-3">
+            <label for="libelle" class="form-label">Libellé de la salle</label>
+            <input type="text" class="form-control" id="libelle" placeholder="libelle">
+        </div>
+
+        <label>Type du salle</label>
+        <select class="form-select" aria-label="Default select example">
+            <option selected>Choisir le nom du salle</option>
+            <option value="1">Open-sapce</option>
+            <option value="2">Bueau</option>
+            <option value="3">Salle de reunion</option>
+        </select>
+
+        <div class="mb-3">
+            <label for="capacite" class="form-label">Capacité</label>
+            <input type="number" class="form-control" id="capacite" placeholder="La capacité du salle">
+        </div>
+        
+
+
+
+
+
+
+
+
+    </form>
+
+</div>
